4c5bd51cb91e66d0a056482e0746f9da56a59cea
GDWR <gregory.dwr@gmail.com>
2024-02-23 15:10:41 +0000

    Add nix fmt alejandra

--- flake.nix
+++ flake.nix
@@ -1,23 +1,27 @@
 {
   description = "Generate templated files quickly from your terminal.";
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system:
-      let 
-	      pkgs = nixpkgs.legacyPackages.${system}; 
-      in
-      {
-        packages = rec { 
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        formatter = pkgs.alejandra;
+        packages = rec {
           plate = pkgs.buildGoModule rec {
             pname = "plate";
             version = "v0.0.1";
             src = ./.;
             vendorHash = "sha256-UgLotBPiykyV4mksUuX3697et2iTJVOYroL8C5Wd9qg=";
-            ldflags = [ "-X 'main.Version=${version}'" ];
+            ldflags = ["-X 'main.Version=${version}'"];
           };
           default = plate;
         };
         devShells.default = pkgs.mkShell {
-          packages = [ pkgs.go pkgs.gopls ];
+          packages = [pkgs.go pkgs.gopls];
         };
       }
     );
