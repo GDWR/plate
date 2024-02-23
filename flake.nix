aaa7ec3d276349cbe56147d9a1952592916b51f3
GDWR <gregory.dwr@gmail.com>
2024-02-23 16:15:32 +0000

    Remove flake-utils and add check on pull request

--- flake.nix
+++ flake.nix
@@ -1,28 +1,27 @@
 {
   description = "Generate templated files quickly from your terminal.";
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        formatter = pkgs.alejandra;
-        packages = rec {
-          plate = pkgs.buildGoModule rec {
-            pname = "plate";
-            version = "v0.0.1";
-            src = ./.;
-            vendorHash = "sha256-UgLotBPiykyV4mksUuX3697et2iTJVOYroL8C5Wd9qg=";
-            ldflags = ["-X 'main.Version=${version}'"];
-          };
-          default = plate;
-        };
-        devShells.default = pkgs.mkShell {
-          packages = [pkgs.go pkgs.gopls];
-        };
-      }
-    );
+
+  outputs = { nixpkgs, ... }: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ] (system: function nixpkgs.legacyPackages.${system});
+  in rec {
+    packages = forAllSystems (pkgs: rec {
+      default = plate;
+      plate = pkgs.buildGoModule rec {
+        pname = "plate";
+        version = "v0.0.1";
+        src = ./.;
+        vendorHash = "sha256-JFrzxduL0Wr3+CGfAmJbAcaCWRP/vLF6nQWds2aamtw=";
+        ldflags = ["-X 'main.Version=${version}'"];
+      };
+    });
+    devShells= forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        packages = [pkgs.go pkgs.gopls];
+      };
+    });
+  };
 }
